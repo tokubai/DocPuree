b2160f521b0f5d21f0853f64ca6f5800b3c13095
“masato1230” <0ht3851f11t426k@ezweb.ne.jp>
2022-12-19 11:24:53 +0900

    Add DocPureeMoshiSerializer

---
+++ docpuree/src/main/java/jp/co/tokubai/docpuree/DocPureeMoshiSerializer.kt
@@ -0,0 +1,18 @@
+package jp.co.tokubai.docpuree
+
+import com.cookpad.puree.PureeSerializer
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+
+class DocPureeMoshiSerializer : PureeSerializer {
+
+    private val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
+
+    override fun serialize(value: Any): String {
+        val adapter = moshi.adapter(value.javaClass)
+        val json = adapter.toJson(value)
+        LogClassHistorySource.serializedClassHistory[value.javaClass.simpleName] = json
+
+        return json
+    }
+}
---
+++ docpuree/src/main/java/jp/co/tokubai/docpuree/LogHistorySource.kt
@@ -0,0 +1,6 @@
+package jp.co.tokubai.docpuree
+
+object LogClassHistorySource {
+    // Map of serialized Class to Json
+    val serializedClassHistory = mutableMapOf<String, String>()
+}
