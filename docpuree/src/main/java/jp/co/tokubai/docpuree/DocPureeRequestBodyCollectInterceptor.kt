01ab2da0b14882f862f512d56762c89868fe5630
“masato1230” <0ht3851f11t426k@ezweb.ne.jp>
2022-12-19 11:14:46 +0900

    Add DocPureeRequestBodyCollectInterceptor

---
+++ docpuree/src/main/java/jp/co/tokubai/docpuree/DocPureeRequestBodyCollectInterceptor.kt
@@ -0,0 +1,20 @@
+package jp.co.tokubai.docpuree
+
+import android.util.Log
+import okhttp3.Interceptor
+import okhttp3.Response
+import okio.Buffer
+
+
+object DocPureeRequestBodyCollectInterceptor : Interceptor {
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request()
+        val buffer = Buffer()
+        request.body?.writeTo(buffer)
+
+        Log.d("RequestBody", buffer.readUtf8())
+
+        return chain.proceed(request)
+    }
+}
