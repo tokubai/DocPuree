5944f26a037c08548879243163c4919ce6bf99a7
“masato1230” <0ht3851f11t426k@ezweb.ne.jp>
2022-12-27 16:43:46 +0900

    logging toast

--- sample/src/main/java/jp/co/tokubai/docpuree/MainActivity.kt
+++ sample/src/main/java/jp/co/tokubai/docpuree/MainActivity.kt
@@ -2,6 +2,7 @@ package jp.co.tokubai.docpuree
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
@@ -9,9 +10,7 @@ import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.cookpad.puree.Puree
 import jp.co.tokubai.docpuree.log.entities.*
@@ -19,6 +18,12 @@ import jp.co.tokubai.docpuree.ui.DocPureeActivity
 import jp.co.tokubai.docpuree.ui.theme.DocPureeTheme
 
 class MainActivity : ComponentActivity() {
+
+    private fun showNotifyLoggedToast(loggedClass: Class<*>) {
+        Toast.makeText(this, loggedClass.simpleName + "is on Logging queue", Toast.LENGTH_SHORT)
+            .show()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -43,35 +48,50 @@ class MainActivity : ComponentActivity() {
                         }
 
                         Button(
-                            onClick = { Puree.send(OnClickFirst()) },
+                            onClick = {
+                                Puree.send(OnClickFirst())
+                                showNotifyLoggedToast(OnClickFirst::class.java)
+                            },
                             modifier = Modifier.fillMaxWidth(),
                         ) {
                             Text(text = "1st")
                         }
 
                         Button(
-                            onClick = { Puree.send(OnClickSecond()) },
+                            onClick = {
+                                Puree.send(OnClickSecond())
+                                showNotifyLoggedToast(OnClickSecond::class.java)
+                            },
                             modifier = Modifier.fillMaxWidth(),
                         ) {
                             Text(text = "2nd")
                         }
 
                         Button(
-                            onClick = { Puree.send(OnClickThird()) },
+                            onClick = {
+                                Puree.send(OnClickThird())
+                                showNotifyLoggedToast(OnClickThird::class.java)
+                            },
                             modifier = Modifier.fillMaxWidth(),
                         ) {
                             Text(text = "3rd")
                         }
 
                         Button(
-                            onClick = { Puree.send(OnClickFourth()) },
+                            onClick = {
+                                Puree.send(OnClickFourth())
+                                showNotifyLoggedToast(OnClickFourth::class.java)
+                            },
                             modifier = Modifier.fillMaxWidth(),
                         ) {
                             Text(text = "4th")
                         }
 
                         Button(
-                            onClick = { Puree.send(OnClickFifth()) },
+                            onClick = {
+                                Puree.send(OnClickFifth())
+                                showNotifyLoggedToast(OnClickFirst::class.java)
+                            },
                             modifier = Modifier.fillMaxWidth(),
                         ) {
                             Text(text = "5th")
@@ -80,18 +100,5 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    DocPureeTheme {
-        Greeting("Android")
     }
 }
