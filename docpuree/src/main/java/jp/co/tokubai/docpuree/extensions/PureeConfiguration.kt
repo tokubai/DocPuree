1eb117e0c49f7a8bc2753017332d855458bd5323
“masato1230” <0ht3851f11t426k@ezweb.ne.jp>
2022-12-28 15:43:16 +0900

    Remove meaningless debug mode check

--- docpuree/src/main/java/jp/co/tokubai/docpuree/extensions/PureeConfiguration.kt
+++ docpuree/src/main/java/jp/co/tokubai/docpuree/extensions/PureeConfiguration.kt
@@ -1,9 +1,7 @@
 package jp.co.tokubai.docpuree.extensions
 
-import android.util.Log
 import com.cookpad.puree.PureeConfiguration
 import com.cookpad.puree.outputs.PureeOutput
-import jp.co.tokubai.docpuree.BuildConfig
 import jp.co.tokubai.docpuree.model.LogDocItem
 import jp.co.tokubai.docpuree.source.DocSource
 
@@ -12,9 +10,6 @@ fun PureeConfiguration.Builder.register(
     output: PureeOutput,
     logDocItem: LogDocItem,
 ): PureeConfiguration.Builder {
-    if (BuildConfig.DEBUG) {
-        Log.d("DocItem", logDocItem.toString())
-        DocSource.logDocSet.add(logDocItem)
-    }
+    DocSource.logDocSet.add(logDocItem)
     return register(logClass, output)
 }
